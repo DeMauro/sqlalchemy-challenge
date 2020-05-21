5913a0f8564a19a2f8d4dc85db19f34a23837b6f
DeMauro <frankdemauro@gmail.com>
2020-05-21 10:03:01 -0600

    final edits

--- climate_app.py
+++ climate_app.py
@@ -20,45 +20,50 @@ app = Flask(__name__)
 #  Home - List all routes that are available.
 @app.route("/")
 def home():
-    print("Server received request for 'Home' page...")
+    print("Take me 'Home' JSON road...")
     return (
-        f"Welcome to the Hawaii Vacation Precipitation Home Page!<br/>"
-        f"Available Routes:<br/>"
+        f"Welcome to the Hawaii Vacation Precipitation Home Page!<br/><br/>"
+        f"Available Routes:<br/><br/>"
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
-        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/tobs<br/><br/>"
+        f"Replace 'yyyy-mm-dd' with year, month, and day information<br/>"
         f"/api/v1.0/yyyy-mm-dd/<br/>"
         f"/api/v1.0/yyyy-mm-dd/yyyy-mm-dd<br/>"
     )
+
+# since both the precipitation and tobs routs need the last year of data I've moved the variables outside of the functions so they can be used in both.
+session = Session(engine)    
+# station with the highest number of observations (from part 1) with cool adjustment
+Bigguy = session.query(Measurement.station, func.count(Measurement.station)).group_by(Measurement.station)\
+        .order_by(func.count(Measurement.station).desc()).all()[0][0]
+# The last day of the report (also from part 1)
+l_day = session.query(Measurement.date).order_by(Measurement.date.desc()).first()[0]
+# the day 1 year prior to the last day
+f_day = datetime.strptime(l_day, '%Y-%m-%d') - dt.timedelta(days=365)
+f_day = f_day.strftime('%Y-%m-%d')
+session.close()
+
 # Convert the query results to a dictionary using `date` as the key and `prcp` as the value
 @app.route("/api/v1.0/precipitation")
 def precipitation():
-    print("This is all of the data.  Did you want the year only?...")
+    print(f"Rain, Rain Go Away...{f_day} to {l_day}")
 
     # Query all dates and precipitation values
     session = Session(engine)
-    results = session.query(Measurement.date,Measurement.prcp).all()
+    results = session.query(Measurement.date,Measurement.prcp)\
+        .filter(Measurement.date.between(f_day, l_day)).all()
     session.close()
     
-    # this gives the results, the below also does the same choose your poison
     p_dict = dict(results)
     return jsonify(p_dict)  
-
-    # # Create a dictionary from the row data and append to a list of all_dates
-    # all_dates = []
-    # for date, prcp in results:
-    #     date_dict = {}
-    #     date_dict["date"] = date
-    #     date_dict["precipitation"] = prcp
-    #     all_dates.append(date_dict)
-    # return jsonify(all_dates)
 
 # Return a JSON list of stations from the dataset.
 @app.route("/api/v1.0/stations")
 def station():
     print("STATION!") #Excellent!
 
-    # Query all dates and precipitation values
+    # Query all stations
     session = Session(engine)
     sresults = session.query(Station.station).all()
     session.close()
@@ -70,18 +75,9 @@ def station():
 # Query the dates and temperature observations of the most active station for the last year of data
 @app.route("/api/v1.0/tobs")
 def tobs():
-    print("Temperatures for the last year")
-    session = Session(engine)
+    print(f"{Bigguy} temperatures for the last year")
     
-    # station with the highest number of observations (from part 1) with cool adjustment
-    Bigguy = session.query(Measurement.station, func.count(Measurement.station)).group_by(Measurement.station)\
-        .order_by(func.count(Measurement.station).desc()).all()[0][0]
-    # The last day of the report (also from part 1)
-    l_day = session.query(Measurement.date).order_by(Measurement.date.desc()).first()[0]
-    # the day 1 year prior to the last day
-    date_object = datetime.strptime(l_day, '%Y-%m-%d')
-    f_day = date_object - dt.timedelta(days=365)
-    f_day = f_day.strftime('%Y-%m-%d')
+    session = Session(engine)
     # the data for the station with the most measurments for one year from the last day of reporting
     year_temps = session.query(Measurement.date,Measurement.prcp)\
         .filter(Measurement.date.between(f_day, l_day))\
@@ -94,6 +90,7 @@ def tobs():
     # for dates after the start date
 @app.route("/api/v1.0/<start_date>/")
 def from_start_date(start_date):
+    print("Min, Avg, and Max Temperature from given date to end date of reporting")
     session = Session(engine)
 
     # from the function in the jupyter notebook
@@ -102,9 +99,10 @@ def from_start_date(start_date):
     session.close()
     return jsonify(MinAvgMax) 
 
-# for dates between the start and end date inclusive
+    # for dates between the start and end date inclusive
 @app.route("/api/v1.0/<start_date>/<end_date>")
 def from_start_to_end_date(start_date, end_date):
+    print("Min, Avg, and Max Temperature for given date range")
     session = Session(engine)
 
     # from the function in the jupyter notebook
@@ -113,10 +111,7 @@ def from_start_to_end_date(start_date, end_date):
 
     session.close()
 
-    return jsonify(MinAvgMax)  
-
-
-
+    return jsonify(MinAvgMax) 
 
 if __name__ == "__main__":
     app.run(debug=True)
